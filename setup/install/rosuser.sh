55a0fb25be744f0366b7d6c3e11dbc49c881adf0
wn1980 <w.n1980@outlook.com>
2019-08-09 14:55:32 +0700

    add kinetic wall paper

--- setup/install/rosuser.sh
+++ setup/install/rosuser.sh
@@ -6,16 +6,13 @@ apt-get update && apt-get install --no-install-recommends --allow-unauthenticate
 	sudo
 
 # Configure user
-user=rosuser
 passwd=rosuser
 uid=1000
 gid=1000
 
-export USER=$user
 export PASSWD=$passwd
-export UID=$uid
+#export UID=$uid
 export GID=$gid
-export HOME=/home/$USER
 
 groupadd $USER && \
     useradd --create-home --no-log-init -g $USER $USER && \
@@ -23,6 +20,6 @@ groupadd $USER && \
     echo "$PASSWD:$PASSWD" | chpasswd && \
     chsh -s /bin/bash $USER && \
     # Replace 1000 with your user/group id
-    usermod  --uid $UID $USER && \
+    #usermod  --uid $UID $USER && \
     groupmod --gid $GID $USER
 
