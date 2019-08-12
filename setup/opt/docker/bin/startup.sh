ca5157c45db64b85288ad513f5577ddd6d1838f3
wn1980 <w.n1980@outlook.com>
2019-08-13 03:41:06 +0700

    done custom-supervisord.conf again

--- setup/opt/docker/bin/startup.sh
+++ setup/opt/docker/bin/startup.sh
@@ -7,12 +7,12 @@ if [ -n "$VNC_PASSWORD" ]; then
     echo -n "$VNC_PASSWORD" > $HOME/.vnc/password1
     x11vnc -storepasswd $(cat $HOME/.vnc/password1) $HOME/.vnc/password2
     chmod 444 $HOME/.vnc/password*
-    sed -i 's/^command=x11vnc.*/& -rfbauth %(ENV_HOME)s\/\.vnc\/password2/' /opt/docker/supervisord.conf
+    sed -i 's/^command=x11vnc.*/& -rfbauth %(ENV_HOME)s\/\.vnc\/password2/' /opt/docker/custom-supervisord.conf
     export VNC_PASSWORD=
 fi
 
 if [ -n "$VNC_RESOLUTION" ]; then
-    sed -i "s/1024x768/$VNC_RESOLUTION/" /opt/docker/bin/xvfb.sh
+    sed -i "s/1024x768/$VNC_RESOLUTION/" /opt/docker/bin/display1.sh
 else
 	export VNC_RESOLUTION=1024x768
 fi
@@ -20,4 +20,4 @@ fi
 /opt/docker/bin/chrome-init.sh
 
 # the rest of alls
-exec /opt/docker/bin/tini-$(uname -m) -- /usr/bin/supervisord -n -c /opt/docker/supervisord.conf
+exec /opt/docker/bin/tini-$(uname -m) -- /usr/bin/supervisord -n -c /opt/docker/custom-supervisord.conf
