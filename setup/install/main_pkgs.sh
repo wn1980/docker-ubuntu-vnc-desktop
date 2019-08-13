2861ea82ba759da38f96ec1dd16d59034b277fce
wn1980 <w.n1980@outlook.com>
2019-08-13 23:44:02 +0700

    can use catkin_make in Dockerfile now

--- setup/install/main_pkgs.sh
+++ setup/install/main_pkgs.sh
@@ -4,6 +4,7 @@ set -e
     
 apt-get update && apt-get install --no-install-recommends --allow-unauthenticated -y \
     software-properties-common \
+    build-essential \
     curl \
     wget \
 	supervisor \
@@ -11,8 +12,6 @@ apt-get update && apt-get install --no-install-recommends --allow-unauthenticate
     jwm \
     x11vnc \
     xvfb \
-    ttf-ubuntu-font-family \
-    build-essential \
     libgl1-mesa-glx \
     libgl1-mesa-dri \
     mesa-utils \
@@ -28,6 +27,7 @@ apt-get update && apt-get install --no-install-recommends --allow-unauthenticate
     usbutils \
     inetutils-ping \
     net-tools \
+    netatalk \
     alsa \
     alsa-utils \
     alsa-tools \
@@ -37,22 +37,8 @@ apt-get update && apt-get install --no-install-recommends --allow-unauthenticate
     python-pip \
     python3-pip \
     keyboard-configuration \
+    ttf-ubuntu-font-family \
     xfonts-base \
     xfonts-thai \
     && rm -rf /var/lib/apt/lists/*
-
-#cp /usr/share/applications/terminator.desktop /root/Desktop
-
-   	#openssh-server \
-    #nginx \
-    #arc-theme \
-    #okular \
-    #pinta \
-    #gtk2-engines-murrine \
-    #gtk2-engines-pixbuf \
-    #gtk2-engines-murrine \
-    #gnome-themes-standard \
-    #terminator \
-    #lxde \
-    #pwgen \
-    #vim-tiny \
+    
