3eef1acdb6e9100850dd755aaeb0d8485e0f6596
wn1980 <w.n1980@outlook.com>
2019-08-13 10:21:48 +0700

    add update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $(uname -m) == 'x86_64' ] 
+then
+	tag=
+elif [ $(uname -m) == 'aarch64' ] 
+then 
+	tag=:rpi
+else
+	echo 'not matched platform!'
+	exit 0
+fi
+
+docker pull wn1980/w-ros${tag}
+
+./run.sh
