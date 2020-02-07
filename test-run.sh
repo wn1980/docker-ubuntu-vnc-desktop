ceecc1d5c1855020f7c853aca978a5fd34a96ee9
wn1980 <w.n1980@outlook.com>
2020-02-08 06:55:07 +0700

    add test-run

---
+++ test-run.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+p1080=1920x1080
+p720=1280x720
+p169=1600x900
+
+if [ $(uname -m) == 'x86_64' ] 
+then
+	tag=
+elif [ $(uname -m) == 'aarch64' ] 
+then 
+	tag=:rpi
+else
+	echo 'not matched platform!'
+	exit 0
+fi
+
+if [[ -f "~/rosuser_home" ]]
+then
+	mkdir -p "~/rosuser_home"
+	chmod -Rf 777 "~/rosuser_home"
+fi
+
+NAME=w-ros-daemon
+
+docker rm -f $NAME
+
+docker system prune -f
+
+docker run -d --name $NAME \
+	--network host \
+	--privileged \
+	--restart unless-stopped \
+	-v /dev:/dev \
+	-v /run/systemd:/run/systemd \
+	-v /etc/localtime:/etc/localtime:ro \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	-e VNC_PASSWORD=vnc123 \
+	-e VNC_RESOLUTION=$p1080 \
+	wn1980/w-ros-novnc${tag} startup.sh
+
+#	-v ./Documents:$HOME/Documents:rw \
